5d0c614eb67fb0f05f76332f6ac0e118cdc955e3
melek <malek.abb55@gmail.com>
2026-04-08 16:37:08 +0200

    ipc en udp

--- network/include/medievail_net/app.h
+++ network/include/medievail_net/app.h
@@ -1,4 +1,3 @@
 ﻿// Gabarit pour l’API publique du démon réseau.
 // On y décrira plus tard NetAppConfig (endpoint IPC, identifiant joueur, liste de pairs)
 // ainsi que la signature de net_app_run.
-
